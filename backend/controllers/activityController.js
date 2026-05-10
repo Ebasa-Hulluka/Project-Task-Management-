8b56d35976193efb358d4f7bda70c1cda7eef757
Ebasa Hulluka Fayisa <ebasahuluka1@gmail.com>
2026-05-10 13:15:40 +0300

    Update activityController.js

--- backend/controllers/activityController.js
+++ backend/controllers/activityController.js
@@ -1,7 +1,7 @@
 const Activity = require("../models/Activity");
 
 // @desc    Get recent activity for logged-in user
-// @route   GET /api/actity
+// @route   GET /api/actty
 // @access  Private
 const getUserActivity = async (req, res) => {
   try {
