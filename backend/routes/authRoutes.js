d36e82f7bb1e93064f9ae0d3eb6c493c8d6965e5
Ebasa Hulluka Fayisa <ebasahuluka1@gmail.com>
2026-05-11 10:45:50 +0300

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -13,7 +13,7 @@ const {
 
 router.post("/login", loginUser);
 
-// Protected Rtes
+// Protected Routes
 router.get("/profile", protect, getUserProfile);
 router.put("/profile", protect, updateUserProfile);
 router.post("/change-password", protect, changeUserPassword);
